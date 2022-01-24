1c5c59ada2b2fe09d2f1a64cc51e68c4db7c5a50
Axhar85 <azhararshad_ntu@yahoo.com>
2022-01-24 17:07:49 +0100

    creating keyborad element

--- src/components/SimpleKeyboard.vue
+++ src/components/SimpleKeyboard.vue
@@ -5,13 +5,29 @@
 <script setup>
 import Keyboard from "simple-keyboard";
 import "simple-keyboard/build/css/index.css";
+import { ref, onMounted, watch } from "vue";
+
+
+const emit = defineEmits(["onKeyPress"]);
 
 import { ref, onMounted, watch } from "vue";
 
 const keyboard = ref(null);
+const onKeyPress = (button) => {
+  emit("onKeyPress", button);
+};
 
 onMounted(() => {
-  keyboard.value = new Keyboard();
+  keyboard.value = new Keyboard("simple-keyboard", {
+    layout: {
+      default: [
+        "q w e r t y u i o p",
+        "a s d f g h j k l",
+        "{enter} z x c v b n m {bksp}",
+      ],
+    },
+    onKeyPress: onKeyPress,
+  });
 });
 
 </script>
