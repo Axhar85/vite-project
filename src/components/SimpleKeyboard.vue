d3382e49756d675e69fc5731a2b4a573bd4c198f
Axhar85 <azhararshad_ntu@yahoo.com>
2022-01-24 16:47:06 +0100

    creating keyborad element

---
+++ src/components/SimpleKeyboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="simple-keyboard"> </div>
+</template>
+
+<script setup>
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+
+import { ref, onMounted, watch } from "vue";
+
+const keyboard = ref(null);
+
+onMounted(() => {
+  keyboard.value = new Keyboard();
+});
+
+</script>
